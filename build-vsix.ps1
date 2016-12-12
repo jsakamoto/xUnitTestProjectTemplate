806c2c10c6a699384ca0f2b316f9f22b0ae4573e
jsakamoto <junichi.jp@gmail.com>
2016-12-12 23:39:30 +0900

    Upgrade: Vsix ver.3 format.

--- build-vsix.ps1
+++ build-vsix.ps1
@@ -17,10 +17,37 @@ $zip.Dispose()
 $manifest = [xml](cat .\extension.vsixmanifest -Raw)
 $ver = $manifest.PackageManifest.Metadata.Identity.Version
 
+# Create "manifest.json".
+$baseDir = (pwd).Path
+$srcFiles = @(ls (Join-Path $baseDir "ProjectTemplates") -Recurse -File | % { $_.FullName })
+$srcFiles += @("icon.png", "extension.vsixmanifest") | % { ls (Join-Path $baseDir $_) } | % { $_.FullName }
+$files = $srcFiles | % {
+  @{
+    sha256 = (Get-FileHash $_ -Algorithm SHA256).Hash;
+    fileName = $_.Substring($baseDir.Length).Replace("\","/");
+  }
+}
+
+$manifestJson = @{
+    id = "xUnitProjectTemplate";
+    version = $ver;
+    type =  "Vsix";
+    language = "en-us";
+    vsixId = "xUnitProjectTemplate";
+    extensionDir = "[installdir]\Common7\IDE\Extensions\nx3pocv5.jy3";
+    files = $files;
+    dependencies = @{
+        "Microsoft.VisualStudio.Component.CoreEditor" = "[11.0,16.0)";
+    }
+}
+$manifestJson | ConvertTo-Json -Compress | Out-File "manifest.json" -Encoding utf8
+
 # Create .vsix a package with embedding version information.
 $zip = new-object Ionic.Zip.ZipFile
 $zip.AddFile((Convert-Path '.\`[Content_Types`].xml'), "") > $null
 $zip.AddFile((Convert-Path .\extension.vsixmanifest), "") > $null
+$zip.AddFile((Convert-Path .\manifest.json), "") > $null
+$zip.AddFile((Convert-Path .\catalog.json), "") > $null
 $zip.AddFile((Convert-Path .\icon.png), "") > $null
 #$zip.AddFile((Convert-Path .\release-notes.txt), "") > $null
 $zip.AddDirectory((Convert-Path .\ProjectTemplates), "ProjectTemplates") > $null
